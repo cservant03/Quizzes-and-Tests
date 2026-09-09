f97bf86d230306585a53aed2b787b1cdbc5268b5
cservant03 <cservant@svsu.edu>
2026-09-09 00:09:02 -0400

    Latest build deployed.

--- course/lunr-pretext-search-index.js
+++ course/lunr-pretext-search-index.js
@@ -44,6 +44,78 @@ var ptx_lunr_docs = [
   "number": "4",
   "title": "",
   "body": "  People with sore throats are often tested for strep throat. A study compared the results of a rapid test with the results of a throat culture on the same patients. The results are summarized below.    Strep-Throat Status   Rapid Test  Positive    Rapid Test  Negative   Total    Have Strep Throat  378  23  401    Do Not Have Strep Throat  26  254  280    Total  404  277  681    Give each answer as a reduced fraction or as a decimal rounded to at least four decimal places.    What is the probability that a patient has strep throat, given that the rapid test is positive?    Given that a patient has a negative rapid test, what is the probability that the patient does not have strep throat?    What is the probability that the rapid test is positive, given that a patient has strep throat?     "
+},
+{
+  "id": "sec-quiz-basics-of-voting",
+  "level": "1",
+  "url": "sec-quiz-basics-of-voting.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "Quiz: Basics of Voting",
+  "body": " Quiz: Basics of Voting   Name:      Use the preference schedule to answer the questions.    Number of voters  7  10  3  8  4    1st choice  A  B  C  A  B    2nd choice  B  C  A  C  A    3rd choice  C  A  B  B  C      How many people voted?    How many first-place votes are needed for a majority?    Which candidate, if any, has a majority of the first-place votes?    Candidate A has the most first-place votes. How many additional first-place votes would A need to have a majority, assuming the total number of voters remains unchanged?        Use the preference schedule to complete each pairwise comparison. Then determine whether the election has a Condorcet candidate.    Number of voters  6  6  10    1st choice  A  B  C    2nd choice  B  C  A    3rd choice  C  A  B      In the comparison between A and B, A receives votes and B receives votes.    In the comparison between B and C, B receives votes and C receives votes.    In the comparison between C and A, A receives votes and C receives votes.    Which candidate is the Condorcet candidate?        Four friends are trying to decide what kind of cake to order for a party: cheesecake, chocolate, or yellow. Everyone was asked to vote for their favorite, and the winner under the unknown voting method was chocolate.  Upon hearing the results, two people who had voted for yellow cake changed their votes to move chocolate to the top of their preference lists. The winner was recalculated, and cheesecake was the winner.  Which fairness criterion has been violated in this election?   Monotonicity Criterion  Condorcet Criterion  Independence of Irrelevant Alternatives Criterion  Majority Criterion     "
+},
+{
+  "id": "ex-quiz-basics-of-voting-majority",
+  "level": "2",
+  "url": "sec-quiz-basics-of-voting.html#ex-quiz-basics-of-voting-majority",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  Use the preference schedule to answer the questions.    Number of voters  7  10  3  8  4    1st choice  A  B  C  A  B    2nd choice  B  C  A  C  A    3rd choice  C  A  B  B  C      How many people voted?    How many first-place votes are needed for a majority?    Which candidate, if any, has a majority of the first-place votes?    Candidate A has the most first-place votes. How many additional first-place votes would A need to have a majority, assuming the total number of voters remains unchanged?     "
+},
+{
+  "id": "ex-quiz-basics-of-voting-condorcet",
+  "level": "2",
+  "url": "sec-quiz-basics-of-voting.html#ex-quiz-basics-of-voting-condorcet",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  Use the preference schedule to complete each pairwise comparison. Then determine whether the election has a Condorcet candidate.    Number of voters  6  6  10    1st choice  A  B  C    2nd choice  B  C  A    3rd choice  C  A  B      In the comparison between A and B, A receives votes and B receives votes.    In the comparison between B and C, B receives votes and C receives votes.    In the comparison between C and A, A receives votes and C receives votes.    Which candidate is the Condorcet candidate?     "
+},
+{
+  "id": "ex-quiz-basics-of-voting-monotonicity",
+  "level": "2",
+  "url": "sec-quiz-basics-of-voting.html#ex-quiz-basics-of-voting-monotonicity",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "",
+  "body": "  Four friends are trying to decide what kind of cake to order for a party: cheesecake, chocolate, or yellow. Everyone was asked to vote for their favorite, and the winner under the unknown voting method was chocolate.  Upon hearing the results, two people who had voted for yellow cake changed their votes to move chocolate to the top of their preference lists. The winner was recalculated, and cheesecake was the winner.  Which fairness criterion has been violated in this election?   Monotonicity Criterion  Condorcet Criterion  Independence of Irrelevant Alternatives Criterion  Majority Criterion    "
+},
+{
+  "id": "sec-quiz-choice-based-voting",
+  "level": "1",
+  "url": "sec-quiz-choice-based-voting.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "Quiz: Choice-Based Voting Methods",
+  "body": " Quiz: Choice-Based Voting Methods   Name:      Find the winner of this election under the plurality method.    Number of voters  6  8  6  9  4  7    1st choice  C  A  B  B  A  C    2nd choice  A  B  C  A  C  B    3rd choice  B  C  A  C  B  A    Winner =       The table below summarizes the results of an approval vote between four options: A, B, C, and D. Each column shows the number of people with a certain approval vote. Approvals are marked with an X.    Number of voters  21  19  17  24  22  22    A    X   X  X    B  X  X   X      C    X  X   X    D   X   X  X     Find the winner under approval voting.      A class is choosing where to go to lunch during a school field trip. The preference schedule for their choices is shown below.    Number of voters  12  13  8  16  10    1st choice  Hardees  Panda Express  Panda Express  Hardees  Subway    2nd choice  Panda Express  Hardees  Subway  Subway  Panda Express    3rd choice  Subway  Subway  Hardees  Panda Express  Hardees    Find the winner of this election under Instant Runoff Voting (IRV), also called Ranked-Choice Voting. Also give the final vote total for the winner.    "
+},
+{
+  "id": "ex-quiz-choice-based-voting-plurality",
+  "level": "2",
+  "url": "sec-quiz-choice-based-voting.html#ex-quiz-choice-based-voting-plurality",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  Find the winner of this election under the plurality method.    Number of voters  6  8  6  9  4  7    1st choice  C  A  B  B  A  C    2nd choice  A  B  C  A  C  B    3rd choice  B  C  A  C  B  A    Winner =    "
+},
+{
+  "id": "ex-quiz-choice-based-voting-approval",
+  "level": "2",
+  "url": "sec-quiz-choice-based-voting.html#ex-quiz-choice-based-voting-approval",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  The table below summarizes the results of an approval vote between four options: A, B, C, and D. Each column shows the number of people with a certain approval vote. Approvals are marked with an X.    Number of voters  21  19  17  24  22  22    A    X   X  X    B  X  X   X      C    X  X   X    D   X   X  X     Find the winner under approval voting.   "
+},
+{
+  "id": "ex-quiz-choice-based-voting-irv",
+  "level": "2",
+  "url": "sec-quiz-choice-based-voting.html#ex-quiz-choice-based-voting-irv",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "",
+  "body": "  A class is choosing where to go to lunch during a school field trip. The preference schedule for their choices is shown below.    Number of voters  12  13  8  16  10    1st choice  Hardees  Panda Express  Panda Express  Hardees  Subway    2nd choice  Panda Express  Hardees  Subway  Subway  Panda Express    3rd choice  Subway  Subway  Hardees  Panda Express  Hardees    Find the winner of this election under Instant Runoff Voting (IRV), also called Ranked-Choice Voting. Also give the final vote total for the winner.   "
 }
 ]
 
